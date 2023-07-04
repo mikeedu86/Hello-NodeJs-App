6ac42bcb4dcf77c04d0024277d3c202d3ea96ccc
UsamaSam <usamarehman87@gmail.com>
2023-07-04 16:01:48 +0500

    html commit

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const http = require('http');
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 5000;
 
+/*
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   var egg='hehe'
   const msg = 'Hello Node!  ' + egg + '\n'
   res.end(msg);
+});
+*/
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(port, () => {
