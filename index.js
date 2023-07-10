ae582c88ab8c0be3a68f45291fb1ce62c9e681d2
UsamaSam <usamarehman87@gmail.com>
2023-07-10 13:07:11 +0500

    server to app

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
